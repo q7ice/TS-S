7adefce6d7695644ac37a66706ef267ea796db46
Raman Valasevich <volosevich.roma@gmail.com>
2021-11-17 14:45:00 +0300

    Changes

--- src/router.js
+++ src/router.js
@@ -7,27 +7,27 @@ const { TestController } = require('./controllers/test.controller');
 
 const router = new Router();
 
-router.post('/is-available-email', emailValidation, AuthController.isAvailableEmail);
-router.post('/registration', emailValidation, passwordValidation, AuthController.registration);
-router.post('/login', emailValidation, passwordValidation, AuthController.login);
-router.post('/logout', AuthController.logout);
+router.post('/api/is-available-email', emailValidation, AuthController.isAvailableEmail);
+router.post('/api/registration', emailValidation, passwordValidation, AuthController.registration);
+router.post('/api/login', emailValidation, passwordValidation, AuthController.login);
+router.post('/api/logout', AuthController.logout);
 
-router.put('/change-name', tokenValidation, UserSettingsController.changeName);
-router.put('/change-email', tokenValidation, emailValidation, UserSettingsController.changeEmail);
-router.put('/change-password', tokenValidation, UserSettingsController.changePassword);
-router.get('/personal-data', tokenValidation, UserSettingsController.getPersonalData);
-router.get('/getAllTests', tokenValidation, TestController.getAll);
-router.get('/validate', tokenValidation, AuthController.validate);
+router.put('/api/change-name', tokenValidation, UserSettingsController.changeName);
+router.put('/api/change-email', tokenValidation, emailValidation, UserSettingsController.changeEmail);
+router.put('/api/change-password', tokenValidation, UserSettingsController.changePassword);
+router.get('/api/personal-data', tokenValidation, UserSettingsController.getPersonalData);
+router.get('/api/getAllTests', tokenValidation, TestController.getAll);
+router.get('/api/validate', tokenValidation, AuthController.validate);
 
-router.post('/getOne', tokenValidation, TestController.getOne);
+router.post('/api/getOne', tokenValidation, TestController.getOne);
 
-router.post('/createNewTest', tokenValidation, TestController.createTest);
-router.post('/editTest', tokenValidation, TestController.editTest);
-router.post('/destroyTest', tokenValidation, TestController.destroyTest);
-router.post('/takeTest', tokenValidation, TestController.takeTest);
-router.post('/changeOpenTest', tokenValidation, TestController.changeOpenTest);
-router.post('/saveAnswers', tokenValidation, TestController.saveAnswers);
-router.post('/getResults', tokenValidation, TestController.getResults);
+router.post('/api/createNewTest', tokenValidation, TestController.createTest);
+router.post('/api/editTest', tokenValidation, TestController.editTest);
+router.post('/api/destroyTest', tokenValidation, TestController.destroyTest);
+router.post('/api/takeTest', tokenValidation, TestController.takeTest);
+router.post('/api/changeOpenTest', tokenValidation, TestController.changeOpenTest);
+router.post('/api/saveAnswers', tokenValidation, TestController.saveAnswers);
+router.post('/api/getResults', tokenValidation, TestController.getResults);
 
 const checkWorking = async (ctx) => {
   ctx.body = 'Server is working';
